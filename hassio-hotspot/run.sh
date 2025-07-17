9bd7faff7b3c5e6137a3a51182bf41c654a0bdac
Daniel Siebke <d.siebke@icloud.com>
2025-07-17 15:55:52 +0200

    Improved and fixed run.sh

--- hassio-hotspot/run.sh
+++ hassio-hotspot/run.sh
@@ -2,20 +2,15 @@
 
 # SIGTERM-handler this funciton will be executed when the container receives the SIGTERM signal (when stopping)
 reset_interfaces(){
-    ifdown $INTERFACE_AP
+    ip link set wlan0_ap down
     sleep 1
-    if [[ "$VIF_ENABLE" == true ]]; then
-        iw dev ${INTERFACE_AP} del
-    else
-        ip link set ${INTERFACE_AP} down
-        ip addr flush dev ${INTERFACE_AP}
-    fi
 }
 
 term_handler(){
-    echo "Resseting interfaces and killing Servers"
+    echo "Reseting interfaces and killing Servers"
     kill $DHCPD_PID $HOSTAPD_PID 2>/dev/null
     reset_interfaces
+    iw dev wlan0_ap del 2>/dev/null
     echo "Stopping..."
     exit 0
 }
@@ -33,8 +28,6 @@ CHANNEL=$(jq --raw-output ".channel" $CONFIG_PATH)
 ADDRESS=$(jq --raw-output ".address" $CONFIG_PATH)
 NETMASK=$(jq --raw-output ".netmask" $CONFIG_PATH)
 BROADCAST=$(jq --raw-output ".broadcast" $CONFIG_PATH)
-VIF_ENABLE=$(jq --raw-output ".vif_enable" $CONFIG_PATH)
-INTERFACE=$(jq --raw-output ".interface" $CONFIG_PATH)
 INTERNET_IF=$(jq --raw-output ".internet_interface" $CONFIG_PATH)
 ALLOW_INTERNET=$(jq --raw-output ".allow_internet" $CONFIG_PATH)
 HIDE_SSID=$(jq --raw-output ".hide_ssid" $CONFIG_PATH)
@@ -47,7 +40,7 @@ DHCP_SUBNET=$(jq --raw-output ".dhcp_subnet" $CONFIG_PATH)
 DHCP_ROUTER=$(jq --raw-output ".dhcp_router" $CONFIG_PATH)
 
 LEASE_TIME=$(jq --raw-output ".lease_time" $CONFIG_PATH)
-STATIC_LEASES=$(jq -r '.static_leases[] | "\(.mac),\(.ip),\(.name)"' $CONFIG_PATH)
+STATIC_LEASES=$(jq -r '.static_leases // [] | .[] | "\(.mac),\(.ip),\(.name)"' $CONFIG_PATH)
 
 # Enforces required env variables
 required_vars=(SSID WPA_PASSPHRASE CHANNEL ADDRESS NETMASK BROADCAST)
@@ -58,44 +51,20 @@ for required_var in "${required_vars[@]}"; do
     fi
 done
 
-INTERFACES_AVAILABLE="$(ifconfig -a | grep '^wl' | cut -d ':' -f '1')"
-UNKNOWN=true
-
-if [[ -z ${INTERFACE} ]]; then
-    echo >&2 "Network interface not set. Please set one of the available:"
-    echo >&2 "${INTERFACES_AVAILABLE}"
+if ! (iw list | awk '/valid interface combinations:/,/^$/' | grep '#{ managed } <= 1' && iw list | awk '/valid interface combinations:/,/^$/' | grep '#{ AP } <= 1'); then
+    echo >&2 "Wi-Fi adapter does not support simultaneous Access point and client mode (VIF)."
+    echo >&2 "Please use a Wi-Fi adapter that supports concurrent interfaces (AP + STA) or disable VIF."
     exit 1
 fi
 
-for OPTION in ${INTERFACES_AVAILABLE}; do
-    if [[ ${INTERFACE} == ${OPTION} ]]; then
-        UNKNOWN=false
-    fi
-done
+# Ensure VIF exists
+echo "Creating virtual interface wlan0_ap"
+iw dev wlan0 interface add wlan0_ap type __ap
+sleep 2
 
-if [[ ${UNKNOWN} == true ]]; then
-    echo >&2 "Unknown network interface ${INTERFACE}. Please set one of the available:"
-    echo >&2 "${INTERFACES_AVAILABLE}"
+if ! ip link show wlan0_ap &>/dev/null; then
+    echo "Interface wlan0_ap does not exist, hostapd will fail. Aborting."
     exit 1
-fi
-
-INTERFACE_AP=${INTERFACE}
-
-if [[ ${VIF_ENABLE} == true ]]; then
-    INTERFACE_AP="${INTERFACE}_ap"
-
-    # Check VIF support
-    if ! (iw list | awk '/valid interface combinations:/,/^$/' | grep '#{ managed } <= 1' && iw list | awk '/valid interface combinations:/,/^$/' | grep '#{ AP } <= 1'); then
-        echo >&2 "Wi-Fi adapter does not support simultaneous Access point and client mode (VIF)."
-        echo >&2 "Please use a Wi-Fi adapter that supports concurrent interfaces (AP + STA) or disable VIF."
-        exit 1
-    fi
-
-    # Ensure VIF exists
-    if ! iw dev | grep -q ${INTERFACE_AP}; then
-        echo "Creating virtual interface ${INTERFACE_AP}"
-        iw dev ${INTERFACE} interface add ${INTERFACE_AP} type __ap
-    fi
 fi
 
 # Make wlan0 interface still show up in HA UI
@@ -104,14 +73,15 @@ nmcli connection add type wifi ifname wlan0 con-name dummy-wifi ssid "placeholde
 nmcli connection modify dummy-wifi connection.autoconnect no
 
 echo "Set nmcli managed no"
-nmcli dev set ${INTERFACE_AP} managed no
+nmcli dev set wlan0_ap managed no
+nmcli connection delete "wlan0_ap" 2>/dev/null
 
-echo "Network interface set to ${INTERFACE_AP}"
+echo "Network interface set to wlan0_ap"
 
 # Configure iptables to enable/disable internet
 RULE_3="POSTROUTING -o ${INTERNET_IF} -j MASQUERADE"
-RULE_4="FORWARD -i ${INTERNET_IF} -o ${INTERFACE_AP} -m state --state RELATED,ESTABLISHED -j ACCEPT"
-RULE_5="FORWARD -i ${INTERFACE_AP} -o ${INTERNET_IF} -j ACCEPT"
+RULE_4="FORWARD -i ${INTERNET_IF} -o wlan0_ap -m state --state RELATED,ESTABLISHED -j ACCEPT"
+RULE_5="FORWARD -i wlan0_ap -o ${INTERNET_IF} -j ACCEPT"
 
 echo "Deleting iptables"
 iptables -v -t nat -D $(echo ${RULE_3})
@@ -133,7 +103,7 @@ fi
 HCONFIG="/hostapd.conf"
 
 cat <<EOF > ${HCONFIG}
-interface=${INTERFACE_AP}
+interface=wlan0_ap
 ssid=${SSID}
 wpa_passphrase=${WPA_PASSPHRASE}
 channel=${CHANNEL}
@@ -153,12 +123,9 @@ if test ${HIDE_SSID} = true; then
 fi
 
 # Setup interface
-ip addr add ${ADDRESS}/${NETMASK} broadcast ${BROADCAST} dev ${INTERFACE_AP}
-ip link set ${INTERFACE_AP} up
-
-echo "Resseting interfaces"
-reset_interfaces
-ifup ${INTERFACE_AP}
+ip addr add ${ADDRESS}/${NETMASK} broadcast ${BROADCAST} dev wlan0_ap
+echo "Bringing up wlan0_ap"
+ip link set wlan0_ap up
 sleep 1
 
 if test ${DHCP_SERVER} = true; then
@@ -174,16 +141,17 @@ if test ${DHCP_SERVER} = true; then
     # Setup hdhcpd.conf
     UCONFIG="/etc/udhcpd.conf"
 
-    echo "Setup udhcpd ..."
-    echo "interface    ${INTERFACE_AP}"  >> ${UCONFIG}
-    echo "start        ${DHCP_START}"    >> ${UCONFIG}
-    echo "end          ${DHCP_END}"      >> ${UCONFIG}
-    echo "max_leases   ${MAX_LEASES}"    >> ${UCONFIG}
-    echo "opt dns      ${DHCP_DNS}"      >> ${UCONFIG}
-    echo "opt subnet   ${DHCP_SUBNET}"   >> ${UCONFIG}
-    echo "opt router   ${DHCP_ROUTER}"   >> ${UCONFIG}
-    echo "opt lease    ${LEASE_TIME}"    >> ${UCONFIG}
-    echo ""                              >> ${UCONFIG}
+    echo "Setup udhcpd.conf ..."
+    cat <<EOF > ${UCONFIG}
+interface    wlan0_ap
+start        ${DHCP_START}
+end          ${DHCP_END}
+max_leases   ${MAX_LEASES}
+opt dns      ${DHCP_DNS}
+opt subnet   ${DHCP_SUBNET}
+opt router   ${DHCP_ROUTER}
+opt lease    ${LEASE_TIME}
+EOF
 
     # Add static leases
     while IFS=, read -r mac ip name; do
@@ -198,12 +166,18 @@ fi
 
 sleep 1
 
-echo "Starting HostAP daemon ..."
+echo -e "\n==== Starting HostAP daemon ====\n"
 hostapd ${HCONFIG} & HOSTAPD_PID=$!
+
+sleep 2
+if ! ps -p $HOSTAPD_PID > /dev/null; then
+    echo "hostapd failed to start. Exiting..."
+    term_handler
+fi
 
 while true; do 
     echo "Interface stats:"
-    ifconfig | grep ${INTERFACE_AP} -A6
+    ifconfig | grep wlan0_ap -A6
     echo "DHCP Leases:"
     cat /var/lib/udhcpd/udhcpd.leases
     sleep 3600
